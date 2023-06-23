75d6410245cd8d6b606ca0a8f44cb40cb4b3c000
z <zbattleman@gmail.com>
2023-06-23 15:02:33 -0400

    double loop

--- Lafny/WhileMonad.lean
+++ Lafny/WhileMonad.lean
@@ -4,22 +4,21 @@ open BigOperators
 open Finset
 
 def while_loop_with_invariantM [Monad m] {State Measure : Type _} [WellFoundedRelation Measure]
-      (invariant : State → Prop)
-      (cond : State → Prop)
-      [DecidablePred cond]
-      (meas : State → Measure)
-      (init : {state : State // invariant state})
-      (next : (state : State) → invariant state → cond state →
-        m {newState // invariant newState ∧ WellFoundedRelation.rel (meas newState) (meas state)}) :
+    (invariant : State → Prop)
+    (cond : State → Prop)
+    [DecidablePred cond]
+    (meas : State → Measure)
+    (init : {state : State // invariant state})
+    (next : (state : State) → invariant state → cond state →
+      m {newState // invariant newState ∧ WellFoundedRelation.rel (meas newState) (meas state)}) :
     m {state // invariant state ∧ ¬ cond state} :=
   loop init
 where
   loop : {state : State // invariant state} → m {state // invariant state ∧ ¬ cond state}
     | ⟨state, invState⟩ =>
         if h : cond state then do
-          let foo ← next state invState h
-          have : WellFoundedRelation.rel (meas ↑foo) (meas state) := foo.2.2 
-          loop ⟨foo.1, foo.2.1⟩
+          let ⟨newState, nSinv, _⟩ ← next state invState h
+          loop ⟨newState, nSinv⟩
         else
           pure ⟨state, invState, h⟩
     termination_by loop decreasing stateWithInv => meas stateWithInv.1
@@ -41,8 +40,52 @@ def whileExample' (f : ℕ → ℕ) (hf : ∀ x, Even x → Even (f x)) (n : ℕ
   pure ⟨p, even_p, this⟩
 
 
+
 #eval whileExample' (fun n => 2 * n + 4) (fun n => by simp [parity_simps]) 12
 
 -- forIn {β} [Monad m] (x : ρ) (b : β) (f : α → β → m (ForInStep β)) : m β
 
 #check ForIn
+
+/-
+x = 0
+y = 10
+inv  y > 0
+while (x < 100) {
+  y += 10
+  while (x < 50) {
+    z = 0
+    z += 5
+    y += 1
+    x += 1
+  }
+  x += 1
+
+}
+-/
+def whileExample'' (n : ℕ) :
+  IO {p : ℕ × ℕ × ℕ  // p.1 = 0 ∧ p.2.1 > 0 } := do
+  let ⟨p, _, _⟩ ← while_loop_with_invariantM
+    (invariant := fun p : ℕ × ℕ × ℕ => p.2.1 > 0)
+    (cond := fun p => p.1 < 100)
+    (meas := fun p => p.1)
+    (init := ⟨(n, 10, 0), by simp⟩)
+    (next := fun p inv_p p1_gt =>
+      -- have p1_pos : 0 < p.1 := by _
+      -- have : p.1 - 2 < p.1 := tsub_lt_self p1_pos (by simp)
+      do
+        IO.println s!"foo {p.2}"
+        let y := p.2.1 + 10
+        let ⟨q, _, _⟩ ←  while_loop_with_invariantM
+          (invariant := fun p : ℕ × ℕ × ℕ => True)
+          (cond := fun p => p.1 < 50)
+          (meas := fun p => p.1)
+          (init := ⟨⟨p.1, y, p.2.2⟩, by trivial⟩)
+          (next := fun p _ _ => do
+            IO.println s!"y: {p.2.1} z: {p.2.2}"
+            return ⟨(p.1 + 1, p.2.1 + 1, p.2.2 + 5), by sorry⟩)
+        return ⟨(q.1 + 1, q.2.1 + 10, q.2.2), by sorry⟩)
+  return ⟨p, sorry⟩
+
+
+#eval whileExample'' 5
