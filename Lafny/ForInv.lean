b49cece90f76fe32a88237837ba94fe8e94c1650
z <zbattleman@gmail.com>
2023-06-29 13:57:50 -0400

    listForInv pairs, typeclass a bit broken

--- Lafny/ForInv.lean
+++ Lafny/ForInv.lean
@@ -2,10 +2,8 @@ import Mathlib.Data.Nat.Parity
 import Mathlib.Data.List.Basic
 import Std.Data.List.Lemmas
 
-def proj (x : ForInStep y') :=
-  match x with 
-  | .yield z => z
-  | .done z => z
+import Lafny.Util 
+
 
 -- this mirrors what's in the do unchained paper, not 100% sure 
 -- how it's relevant yet, but I think it's good to have around.
@@ -26,13 +24,28 @@ def myForM_forIn {β : Type _}
   | .error a => pure a
 
 
-#check ForIn
+class Container (α : outParam (Type u)) (γ : Type v) extends Membership α γ where
+  /-- Superset of the membership type class that requires a length -/
+  size : γ → Nat
+
+instance : Container α (List α) where
+  size := List.length
+
+instance : Container α (Finset α) where
+  size := Finset.card
+
+instance : Container α (Option α) where
+  size := fun optx =>
+    match optx with
+    | none => 0
+    | (some _) => 1
+
 
 -- in theory this might be it, just put some fancy syntax around it.
 -- I need to read Metaprogramming in Lean to figure out what to do next
--- here, but this is a very large step I think.
+-- here, but this is a very large step I think. 
 
-class ForInv (m : Type u₁ → Type u₂) (ρ : Type u) (α : outParam (Type v)) [Membership α ρ] where
+class ForInv (m : Type u₁ → Type u₂) (α : outParam (Type u)) (ρ : Type v) where
   /-- `forIn x b f : m β` runs a for-loop in the monad `m` with additional state `β`.
   This traverses over the "contents" of `x`, and passes the elements `a : α` to
   `f : α → β → m (ForInStep β)`. `b : β` is the initial state, and the return value
@@ -53,45 +66,61 @@ class ForInv (m : Type u₁ → Type u₂) (ρ : Type u) (α : outParam (Type v)
     return .yield b)
   ```
   (Here `b` corresponds to the variables mutated in the loop.) -/
-  forInv {β : Type _} [Monad m] 
+  forInv {β : Type u₁} [Monad m] [C : Container α ρ]
     (container : ρ)
-    (invariant : β → Prop) 
-    (b : {st : β // invariant st}) 
-    (next : {a : α // a ∈ container} → {st // invariant st} → m (ForInStep {st // invariant st}))
-    : m {st // invariant st}
+    (invariant : Fin (C.size container).succ → β → Prop) 
+    (b : {st : β // invariant 0 st}) 
+    (next : (i : Fin (C.size container)) → {a : α // a ∈ container} → {st // invariant (Fin.castSucc i) st} → m (ForInStep {st // invariant (Fin.succ i) st}))
+    : m { pr : (Fin (C.size container).succ) × β // invariant pr.1 pr.2}
 
-def listForInv {α : Type _} {β : Type v} {m : Type v → Type w} [Monad m]
-    (xs : List α)
-    (invariant : β → Prop) 
-    (init : {st // invariant st}) 
-    (next : {a // a ∈ xs} → {st // invariant st} → m (ForInStep {out // invariant out})) 
-    : m {st // invariant st} :=
-  let rec @[specialize] loop (L : List α) (hL : ∀ x ∈ L, x ∈ xs) (b : {st // invariant st}) :=
-    match L with
-    | [] => pure b
-    | a::as => do
-        match (← next ⟨a, by exact hL a (by exact (List.mem_cons_self a as))⟩ b) with
-        | ForInStep.done b'  => pure b'
-        | ForInStep.yield b' => loop as (fun x hx => hL x (by simp [hx])) b'
-  loop xs (by simp) init
+def listForInv {α : outParam (Type u)} {β : Type v} {m : Type v → Type w} [Monad m]
+    (L : List α)
+    (invariant : Fin L.length.succ → β → Prop) 
+    (init : {st // invariant 0 st}) 
+    (next : (i : Fin (L.length)) → {x // x ∈ L} → {st // invariant (Fin.castSucc i) st} → m (ForInStep {out // invariant (Fin.succ i) out})) 
+    : m { pr : (Fin L.length.succ) × β // invariant pr.1 pr.2} :=
+  let rec @[specialize] loop : (remaining : List α) → (hRem : ∀ x, x ∈ remaining → x ∈ L) → (i : Fin L.length.succ) → (b : {st // invariant i st}) →
+    (hinv : remaining.length + i = L.length) → m {pr : (Fin L.length.succ) × β // invariant pr.1 pr.2}
+  | [], _, i, state, hinv => pure ⟨(L.length, state), by 
+      rw [List.length_nil, zero_add] at hinv
+      convert state.2
+      ext
+      rw [hinv]
+      simp⟩
+  | x::xs, hRem, i, state, hinv =>
+      have h1: xs.length + (i + 1) = L.length := by 
+        apply Eq.trans _ hinv
+        rw [add_comm _ 1, ←add_assoc, List.length_cons]
+      have h2: (i : ℕ) < L.length :=
+        lt_of_lt_of_eq (Nat.lt_of_succ_le (Nat.le_add_left _ _)) h1
+      let i' : Fin (L.length) := ⟨i, h2⟩
+      do
+      match (← next i' ⟨L[i'], by simp[List.get_mem]⟩ state) with
+      | ForInStep.done b' => 
+          let ⟨b, binv⟩ := b'
+          pure ⟨(Fin.succ i', b), by simpa⟩
+      | ForInStep.yield b' => loop xs (fun x hx => hRem x (by simp[hx])) (Fin.succ i') b' h1
+  loop L (by simp) 0 init (by simp)
 
-def sumOfEven (L : List Nat) (h : ∀ x, x ∈ L → Even x) := Id.run do
-  let mut state : {n : Nat // Even n} := ⟨0, by simp⟩;
-  state ← listForInv L (fun sum => Even sum) state
-    (fun a sum => do 
-      let ⟨st, stInv⟩ := sum
-      let out := st + a.1
-      return .yield ⟨out, by 
-        simp
-        have sumEven : Even st := by simp [stInv]
-        have aEven : Even a.1 := by exact h a.1 a.2
-        apply Nat.even_add.2
-        exact ⟨fun _ => aEven, fun _ => sumEven⟩
-      ⟩
-    )
-  return state
-
-#eval sumOfEven [2, 4] (by simp)
+#check listForInv
 
 instance : ForInv m (List α) α where
   forInv := listForInv
+
+-- def sumOfEven (L : List Nat) (h : ∀ x, x ∈ L → Even x) := Id.run do
+--   let mut state : {n : Nat // Even n} := ⟨0, by simp⟩;
+--   state ← listForInv L (fun sum => Even sum) state
+--     (fun a sum => do 
+--       let ⟨st, stInv⟩ := sum
+--       let out := st + a.1
+--       return .yield ⟨out, by 
+--         simp
+--         have sumEven : Even st := by simp [stInv]
+--         have aEven : Even a.1 := by exact h a.1 a.2
+--         apply Nat.even_add.2
+--         exact ⟨fun _ => aEven, fun _ => sumEven⟩
+--       ⟩
+--     )
+--   return state
+
+-- #eval sumOfEven [2, 4] (by simp)
