a68e010b6a06a8bfd384b48a80724c2a73d70c8e
z <zbattleman@gmail.com>
2023-07-12 17:49:58 -0400

    ex 11.6

--- Dafny-Book-Ex/11.6.lean
+++ Dafny-Book-Ex/11.6.lean
@@ -5,8 +5,9 @@ def ex : {x // x = 102} := Id.run do
     (meas := fun ⟨x, _⟩ => 100 - x)
     (init  := (⟨0, by simp⟩ : {s // s % 3 = 0 ∧ s < 105}))
     (next := fun ⟨s, hs⟩ => do 
-      if h : (s > 100) then
-        return Sum.inl ⟨s, sorry⟩
+      if h : (s < 100) then
+        return Sum.inr ⟨⟨s+3, sorry⟩, by sorry⟩
       else
-        return Sum.inr ⟨⟨s+3, sorry⟩, by sorry⟩)
+        return Sum.inl ⟨s, sorry⟩)
+        
   return κ
